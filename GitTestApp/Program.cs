e131532fbb4d867a6f9d718e8472ec964e8258da
Holger Stegemann <hs@geheim.de>
2020-01-31 12:59:12 +0100

    Warte auf Keypress

--- GitTestApp/Program.cs
+++ GitTestApp/Program.cs
@@ -7,6 +7,8 @@ namespace GitTestApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Git!");
+            Console.WriteLine("Press any key to end program");
+            Console.ReadKey();
         }
     }
 }
