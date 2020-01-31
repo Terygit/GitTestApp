f6523eba736663239250dcf1c7dbdc8dff36de0c
Holger Stegemann <hs@geheim.de>
2020-01-31 12:53:51 +0100

    new user test

--- GitTestApp/Program.cs
+++ GitTestApp/Program.cs
@@ -6,7 +6,7 @@ namespace GitTestApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Git!");
+            Console.WriteLine("Hello, Git!");
         }
     }
 }
